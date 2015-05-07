1dd2d99d6c0b90779b3bd628acf0ca3fd30a5792
gulle <gulle@gulle-PC>
2015-05-07 10:17:49 +0200

    Conectados los metodos con la clase vista

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -17,6 +17,8 @@ public class Superficies {
     public static void main(String[] args) {
         String shapeType;
         Modelo md=new Modelo();
+        Vista vis=new Vista();
+        
         sc = new Scanner(System.in);
 
         System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
@@ -28,7 +30,7 @@ public class Superficies {
             System.out.println("what is the side length?");
             md.setSidelength(sc.nextFloat());
             md.setArea(md.getSidelength()*md.getSidelength());
-            
+            vis.square(md);
         }
         if (shapeType.equals("rectangle")) {
             //add area calculations for rectangle here
@@ -38,7 +40,7 @@ public class Superficies {
             System.out.println("What is the rectangles height?");
             md.setSideheigth(sc.nextFloat());
             md.setArea(md.getSidelength()*md.getSideheigth());
-            
+            vis.rectangle(md);
             
         }
         if (shapeType.equals("triangle")) {
@@ -49,7 +51,7 @@ public class Superficies {
             System.out.println("What is the height of the triangle?");
             md.setSideheigth(sc.nextFloat());
             md.setArea((float)(0.5 * md.getSidelength()* md.getSideheigth()));
-            
+            vis.triangle(md);
 
         }
         if (shapeType.equals("circle")) {
@@ -58,7 +60,7 @@ public class Superficies {
             System.out.println("What is the radius of the circle?");
             md.setSidelength(sc.nextFloat());
             md.setArea((float)(3.14159265 * md.getSidelength()));
-            
+            vis.circle(md);
 
         }
 
